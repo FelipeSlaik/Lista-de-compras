36705b62a9b33cf128f959cf814aa2d32645b1b4
FelipeSlaik <felipeslaik@gmail.com>
2024-08-27 01:04:00 -0300

    Adicionando interatividade e lista de comprados

--- scripts.js
+++ scripts.js
@@ -1,44 +1,89 @@
 const item = document.getElementById("input-item");
 const botaoSalvarItem = document.getElementById("adicionar-item");
 const listaDeCompras = document.getElementById("lista-de-compras");
-
+const listaComprados = document.getElementById("lista-comprados");
+let contador = 0
 botaoSalvarItem.addEventListener("click", adicionarItem);
 
-function adicionarItem(event){
+function adicionarItem(event) {
     event.preventDefault()
-    
+
     const itemDaLista = document.createElement("li");
     const containerItemLista = document.createElement("div");
-    containerItemLista.classList.add("lista-item-container");
-    
+    containerItemLista.classList.add("item-lista-container");
+
     const containerNomeDoItem = document.createElement("div");
+
+    const containerCheckbox = document.createElement("div");
+    containerCheckbox.classList.add("container-checkbox");
+
+    const checkboxInput = document.createElement("input");
+    checkboxInput.type = "checkbox";
+    checkboxInput.classList.add("checkbox-input");
+    checkboxInput.id = "checkbox-" + contador++;
+
+    const checkboxLabel = document.createElement("label");
+    checkboxLabel.setAttribute("for", checkboxInput.id);
+
+    checkboxLabel.addEventListener("click", function (event) {
+        const checkboxInput = event.currentTarget.querySelector(".checkbox-input");
+        const checkboxCustomizado = event.currentTarget.querySelector(".checkbox-customizado");
+        const itemTitulo = event.currentTarget.closest("li").querySelector("#item-titulo");
+        if (checkboxInput.checked) {
+            checkboxCustomizado.classList.add("checked");
+            itemTitulo.style.textDecoration = "line-through";
+            listaComprados.appendChild(itemDaLista);
+        } else {
+            checkboxCustomizado.classList.remove("checked");
+            itemTitulo.style.textDecoration = "none";
+            listaDeCompras.appendChild(itemDaLista);
+        }
+    });
+
+    const checkboxCustomizado = document.createElement("div");
+    checkboxCustomizado.classList.add("checkbox-customizado");
+
+    checkboxLabel.appendChild(checkboxInput);
+    checkboxLabel.appendChild(checkboxCustomizado);
+
+    containerCheckbox.appendChild(checkboxLabel);
+    containerNomeDoItem.appendChild(containerCheckbox);
+
     const nomeDoItem = document.createElement("p");
+    nomeDoItem.id = "item-titulo"
     nomeDoItem.innerText = item.value;
     containerNomeDoItem.appendChild(nomeDoItem);
-    
+
+
     const containerBotoes = document.createElement("div");
     const botaoRemover = document.createElement("button");
     botaoRemover.classList.add("item-lista-button");
 
-    const botaoEditar = document.createElement("button");
-    botaoEditar.classList.add("item-lista-button");
-
     const imagemRemover = document.createElement("img");
-    imagemRemover.src= "img/Excluir.svg";
-    imagemRemover.alt= "Excluir";
-    
-    const imagemEditar = document.createElement("img");
-    imagemEditar.src= "img/Edição.svg";
-    imagemEditar.alt= "Editar";
+    imagemRemover.src = "img/Excluir.svg";
+    imagemRemover.alt = "Excluir";
 
     botaoRemover.appendChild(imagemRemover);
     containerBotoes.appendChild(botaoRemover);
-    
+
+    const botaoEditar = document.createElement("button");
+    botaoEditar.classList.add("item-lista-button");
+
+    const imagemEditar = document.createElement("img");
+    imagemEditar.src = "img/Edição.svg";
+    imagemEditar.alt = "Editar";
+
     botaoEditar.appendChild(imagemEditar);
     containerBotoes.appendChild(botaoEditar);
-    
+
     containerItemLista.appendChild(containerNomeDoItem);
     containerItemLista.appendChild(containerBotoes);
+
+    const itemData = document.createElement("p");
+    itemData.innerText = `${new Date().toLocaleDateString("pt-BR", { weekday: "long" })}(${new Date().toLocaleDateString()}) às ${new Date().toLocaleTimeString("pt-BR", {hour: "numeric", minute: "numeric"})}`;
+    itemData.classList.add("item-lista-texto");
+
     itemDaLista.appendChild(containerItemLista);
-    listaDeCompras.appendChild(itemDaLista)
+    itemDaLista.appendChild(itemData);
+    listaDeCompras.appendChild(itemDaLista);
 }
